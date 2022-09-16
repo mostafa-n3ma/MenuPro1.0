932ba361eb51a548bc7702c42944ba035dfa7e59
mostafa-n3ma <77761759+mostafa-n3ma@users.noreply.github.com>
2022-09-16 09:54:40 +0300

    feat: adding ImagePicker.kt a class that used for opening (ActivityResultLauncher) and can be use to get Uri,Bitmap and display the Chosen image on ImageView live Uri used from activity or fragment

--- app/src/main/java/com/mostafan3ma/android/menupro10/MainActivity.kt
+++ app/src/main/java/com/mostafan3ma/android/menupro10/MainActivity.kt
@@ -1,47 +1,63 @@
 package com.mostafan3ma.android.menupro10
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.graphics.ImageDecoder.decodeBitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.widget.TextView
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.decodeBitmap
 import androidx.lifecycle.coroutineScope
 import com.mostafan3ma.android.menupro10.databinding.ActivityMainBinding
-import com.mostafan3ma.android.menupro10.oporations.data_Entities.NetworkEntity
-import com.mostafan3ma.android.menupro10.oporations.data_Entities.cache_Entities.CacheShop
 import com.mostafan3ma.android.menupro10.oporations.localDataSource.LocalDataSource
 import com.mostafan3ma.android.menupro10.oporations.remoteDataSource.RemoteDataSource
 import com.mostafan3ma.android.menupro10.oporations.repository.ShopRepository
-import com.mostafan3ma.android.menupro10.oporations.utils.DataState
+import com.mostafan3ma.android.menupro10.oporations.utils.ImagePicker
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var repository: ShopRepository
 
     @Inject
-    lateinit var remoteDataSource: RemoteDataSource
-
-    @Inject
-    lateinit var localDataSource: LocalDataSource
+    lateinit var imagePicker: ImagePicker
 
 
     private lateinit var binding: ActivityMainBinding
 
 
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onResume() {
+        super.onResume()
+        if (imagePicker.returnedUri !=null){
+            Toast.makeText(this,imagePicker.returnedUri.toString(),Toast.LENGTH_LONG).show()
+            binding.img.setImageBitmap(imagePicker.getBitmap(this))
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
+        imagePicker.register(this)
 
-       lifecycle.coroutineScope.launch{
+        binding.chooseImageBtn.setOnClickListener {
+            imagePicker.launchRegistrar()
+        }
+
+
+
+
+        lifecycle.coroutineScope.launch {
 //           repository.insertShop(CacheShop(id = "1", name = "shop1"))
 //           repository.refreshCacheDatabase(lifecycle.coroutineScope)
 
@@ -52,9 +68,9 @@ class MainActivity : AppCompatActivity() {
 //                 is DataState.Loading->"Loading"
 //             }
 //         }.launchIn(lifecycle.coroutineScope)
-       }
-
-
+        }
 
     }
+
+
 }
